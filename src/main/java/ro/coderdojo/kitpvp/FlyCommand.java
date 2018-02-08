cde8eb6d3c9b43a3691d7f33151691524e39f6b8
BuildTools <unconfigured@null.spigotmc.org>
2018-02-08 19:03:33 +0200

    Fly Command

--- src/main/java/ro/coderdojo/kitpvp/FlyCommand.java
+++ src/main/java/ro/coderdojo/kitpvp/FlyCommand.java
@@ -3,13 +3,31 @@ package ro.coderdojo.kitpvp;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player Player;
 
 
 public class FlyCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
-		commandSender.sendMessage("Ai reușit să creezi o comandă în Minecraft cu Spigot");
+		Player player = (Player) sender;
+		if (command.getName().equalsIgnoreCase("/fly")) {
+			if(args.length == 0) {
+			    player.setAllowFlight(!player.getAllowFlight());
+			} else if(args.length == 1) {
+				if(args[0].equalsIgnoreCase("on")) {
+					player.setAllowFlight(true);
+				} else if(args[0].equalsIgnoreCase("off")) {
+					player.setAllowFlight(false);
+				} else {
+					// Obviously, the next line could be ignored
+					// and return false; could be used if you set the usage message
+					player.sendMessage("usage: /fly [on:off]");
+					return true;
+				}
+			}
+			player.sendMessage("Fly has been "+(player.getAllowFlight() ? "enabled" : "disabled"));
+		}
 		return false;
 	}
 }
